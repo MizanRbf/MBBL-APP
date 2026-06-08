9a72013ab103c767e65a816ed5706d6484904c95
MizanRbf <mizanrbf@gmail.com>
2026-06-09 05:23:34 +0600

    folder structure created

---
+++ src/types/index.ts
@@ -0,0 +1,13 @@
+export interface Book {
+  id: string;
+  title: string;
+  thumbnail: string;
+  pdfUrl: string;
+}
+
+export interface Video {
+  id: string;
+  title: string;
+  thumbnail: string;
+  videoUrl: string;
+}
