e22dd4fb5de91d247967c28deb870ce5c4f79bf1
MizanRbf <mizanrbf@gmail.com>
2026-06-08 23:07:53 +0600

    bug solved

--- App.tsx
+++ App.tsx
@@ -1,44 +1,25 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
-
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
+import React from 'react';
+import './global.css';
+import { View, Text, StyleSheet } from 'react-native';
+const App = () => {
   return (
     <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
+      <Text style={styles.fontBold}>Hello, Maria</Text>
+      <Text>I Love you jan</Text>
+      <Text>তুমি ঘুমাও গিয়া</Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  fontBold: {
+    fontSize: 24,
+    fontWeight: 'bold',
   },
 });
 
