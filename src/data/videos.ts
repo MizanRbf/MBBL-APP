2bb17af2199fec58e6e04f92adc36dbe6272be7a
MizanRbf <mizanrbf@gmail.com>
2026-09-12 23:36:57 +0600

    new class title added

--- src/data/videos.ts
+++ src/data/videos.ts
@@ -1,7 +1,7 @@
 export const videoCategories = [
   {
     id: '1',
-    title: 'আল-ফারাযদাকের কবিতা',
+    title: 'আল-হামযিয়্যাহ কাসিদা',
     videos: [
       {
         id: '1',
@@ -14,6 +14,16 @@ export const videoCategories = [
         videoUrl: 'https://youtu.be/D5bSq80oVGc',
       },
       {
+        id: '4',
+        title: 'Part-04',
+        videoUrl: 'https://youtu.be/2qccADF77lc',
+      },
+      {
+        id: '5',
+        title: 'Part-05',
+        videoUrl: 'https://youtu.be/2qccADF77lc',
+      },
+      {
         id: '3',
         title: 'Part-03',
         videoUrl: 'https://youtu.be/2qccADF77lc',
@@ -22,7 +32,7 @@ export const videoCategories = [
   },
   {
     id: '2',
-    title: 'আল-মুতানাব্বির কবিতা',
+    title: 'যৌবনকালের আক্ষেপ নিয়ে কাসিদা',
     videos: [
       {
         id: '4',
@@ -39,137 +49,37 @@ export const videoCategories = [
         title: 'Part-03',
         videoUrl: 'https://youtu.be/C0yToPfkp1g',
       },
+      {
+        id: '7',
+        title: 'Part-04',
+        videoUrl: 'https://youtu.be/C0yToPfkp1g',
+      },
+      {
+        id: '8',
+        title: 'Part-05',
+        videoUrl: 'https://youtu.be/C0yToPfkp1g',
+      },
     ],
   },
   {
     id: '3',
-    title: 'ঐতিহাসিক খুতবাহসমূহ',
+    title: 'মাকামাতে হারীরী',
     videos: [
       {
-        id: '7',
-        title: 'কুস ইবনে সাইদা আল ইয়াদি',
-        videoUrl: 'https://youtu.be/Jumdka4XySE',
-      },
-      {
-        id: '8',
-        title: 'বিদায় হজ্জের ভাষণ, Part-01',
-        videoUrl: 'https://youtu.be/uR8PRCkMLrc',
-      },
-      {
         id: '9',
-        title: 'বিদায় হজ্জের ভাষণ, Part - 02',
-        videoUrl: 'https://youtu.be/on904GBExXU',
-      },
-      {
-        id: '10',
-        title: 'বাইয়াত গ্রহণ বাদ আবু বকর রা.-এর ভাষণ',
-        videoUrl: 'https://youtu.be/W70RGLSl74M',
-      },
-      {
-        id: '11',
-        title: 'উমার রা.-এর ভাষণ',
-        videoUrl: 'https://youtu.be/HqqJ7W9pe50',
-      },
-      {
-        id: '12',
-        title: 'উসমান রা.-এর ভাষণ ',
-        videoUrl: 'https://youtu.be/-3KZi7L2QgU',
-      },
-      {
-        id: '13',
-        title: 'আলী রা.-এর ভাষণ',
-        videoUrl: 'https://youtu.be/vRCYkCi76mQ',
-      },
-      {
-        id: '14',
-        title: 'নাবীর মৃত্যুর দিন আবু বকর রা.-এর ভাষণ',
-        videoUrl: 'https://youtu.be/JdrIb5kEPcQ',
-      },
-      {
-        id: '15',
-        title: 'আয়েশা রা. এর ভাষণ',
-        videoUrl: 'https://youtu.be/8AarS7h0P5Q',
-      },
-      {
-        id: '16',
-        title: 'উমার বিন আব্দুল আযীয রহ. এর ভাষণ',
-        videoUrl: 'https://youtu.be/P_PnlKfGNG8',
-      },
-      {
-        id: '17',
-        title: 'ত্বারিক বিন যিয়াদ-এর ভাষণ, Part-01',
-        videoUrl: 'https://youtu.be/n8PzObc0Z4U',
-      },
-      {
-        id: '18',
-        title: 'ত্বারিক বিন যিয়াদ-এর ভাষণ, Part-02',
-        videoUrl: 'https://youtu.be/b14n6l_ck9Q',
-      },
-      {
-        id: '19',
-        title: 'ত্বারিক বিন যিয়াদ-এর ভাষণ, Part-03',
-        videoUrl: 'https://youtu.be/23EiC8WX00M',
-      },
-      {
-        id: '20',
-        title: 'হাসান বসরী র. এর নসিহা, Part-01',
-        videoUrl: 'https://youtu.be/ZbG-_1eC-Mc',
+        title: 'Part-01',
+        videoUrl: 'https://youtu.be/Jumdka4XySE',
       },
     ],
   },
   {
     id: '4',
-    title: 'রাসূলের প্রেরিত পত্রসমূহ',
+    title: 'মাকামাতে হামাদানী',
     videos: [
       {
-        id: '21',
-        title: 'রোম সম্রাট হিরাক্লিয়াসের প্রতি',
+        id: '10',
+        title: 'Part-01',
         videoUrl: 'https://youtu.be/w96YrBwW95g',
-      },
-      {
-        id: '22',
-        title: 'মিশরের সম্রাট মুকাওকিসের প্রতি',
-        videoUrl: 'https://youtu.be/28dKqHdgvPk',
-      },
-      {
-        id: '23',
-        title: 'সিরিয়া অধিপতি হারিসের প্রতি',
-        videoUrl: 'https://youtu.be/mUeYd-EEKRo',
-      },
-      {
-        id: '24',
-        title: 'পারস্য সম্রাট খসরু পারভেজের প্রতি',
-        videoUrl: 'https://youtu.be/DVTqrJOzYfY',
-      },
-      {
-        id: '25',
-        title: 'হাবশার রাজা নাজ্জাশীর প্রতি',
-        videoUrl: 'https://youtu.be/6YccOmfq4sQ',
-      },
-      {
-        id: '26',
-        title: '‘আলা ইবনে হাযরামীর সাথে বাহরায়নবাসীর সন্ধি',
-        videoUrl: 'https://youtu.be/bghpIcJeqHc',
-      },
-      {
-        id: '27',
-        title: 'বাহরায়ন অধিপতি মুনযিরের প্রতি',
-        videoUrl: 'https://youtu.be/pkof-KmKMNE',
-      },
-      {
-        id: '28',
-        title: 'বাহরায়নবাসীর প্রতি',
-        videoUrl: 'https://youtu.be/pWJFixS1ft0',
-      },
-      {
-        id: '29',
-        title: 'লেখকদের প্রতি আব্দুল হামিদ, Part-01',
-        videoUrl: 'https://youtu.be/L7xavHveiVo',
-      },
-      {
-        id: '30',
-        title: 'লেখকদের প্রতি আব্দুল হামিদ, Part-02',
-        videoUrl: 'https://youtu.be/ozJYml9aPdQ',
       },
     ],
   },
